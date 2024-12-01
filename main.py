32588f99388ab8b51974d3cd2748213c8b8f630c
sasa-54 <odubrivna@gmail.com>
2024-12-01 11:41:22 +0200

    created class student

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello world!!!")
+class Student:
+ amount_of_students = 0
+ def __init__(self, height=160):
+     self.height = height
+     Student.amount_of_students += 1
+ def grow(self, height=1):
+     self.height+=height
+
+
+sasha = Student()
+masha = Student(height=170)
+
+sasha.grow(height=15)
+
+print(sasha.height)
+print(masha.height)
+
+print(Student.amount_of_students)
